e334d39a73d215e36a97387254d187729db7db50
Holger Flick <holger@flixengineering.com>
2024-03-13 00:28:55 -0400

    hero-class seems to be undefined

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className=' py-5'>
       <Container className='d-flex justify-content-center'>
-        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+        <Card className='p-5 d-flex flex-column align-items-center bg-light w-75'>
           <h1 className='text-center mb-4'>MERN Authentication</h1>
           <p className='text-center mb-4'>
             This is a boilerplate for MERN authentication that stores a JWT in
